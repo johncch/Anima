8b87204175dd997a4b7e95109bb0455c55a45016
Chong Han Chua <johncch@gmail.com>
2012-04-16 23:07:21 -0700

    Various bug fixes + stability enhancements

--- readme.md
+++ readme.md
@@ -6,23 +6,61 @@ A tiny CSS library for animating DOM elements using CSS transitions.
     
 ## Features
 Anima was created with 2 things in mind:
+
 1. Support for sequential animations
 2. Support for concurrent animations using the concept of a Frame.
 
-For examples, look at the test page located under test/ 
+A simple example is as follows:
+
+    Anima.queue(element, {
+        left: 10,
+        opacity: 0
+    }, 0.4, function() {
+        console.log("completed animation!);
+    });
+    Anima.play();
+    
+Remember that all animations are queued up until the play function is called.
+
+Also, to run animations concurrently
+    Anima.frame([
+        {
+            element: el,
+            properties: {
+            	left: 100
+            },
+            duration: "100ms"
+        }, {
+            element: el2,
+            properties: {
+            	left: 100
+            },
+            duration: "100ms"
+        }
+    ]
+    Anima.play()
+
+
+For more examples, look at the test page located under test/ 
 
                                                                                                                                                           
 ## Warning
 This version can only be considered alpha. There are a few areas that                                                                         
+
 1. Implement a non CSS fallback. CSS changes will be executed immediately.                                                      
 2. Debug for Opera
 3. Test on IE browsers
-4. Document                                                                                
+4. Document          
+
+TODO: Look at all the async functions and resolve them.
                                            
 ## License                                                                      
 (The MIT License)
 
 Copyright (c) 2012 Chong Han Chua (johncch@live.com)
+
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- src/anima.js
+++ src/anima.js
@@ -11,7 +11,7 @@
 // will come in as I test them.
 //
 // author: Chong Han Chua
-// License: BSD
+// License: MIT
 var Anima = (typeof Anima == "undefined") ? {} : Anima;
 
 // Test for vendor extension
@@ -50,12 +50,13 @@ Anima.Constants = (function() {
 				return Anima.Prefix + "TransitionEnd";
 		}
 	})();
+	constants.BackfaceVisibility = Anima.Prefix + "BackfaceVisibility";
 	return constants;
 })();
 
 // The list of currently supported transforms.
 Anima.TransformProperties = ["scale"];
-Anima.CSSProperties = ["top", "left", "opacity"];
+Anima.CSSProperties = ["top", "left", "opacity", "width", "height"];
 
 // Parse transitions take a list of supplied options and parse them into a simplified
 // list of objects.
@@ -92,7 +93,7 @@ Anima.parseTransitions = function(options) {
 
 // @return time - in a format that css understands
 Anima.parseTime = function(time, def) {
-	if (typeof time == "undefined") {
+	if (!time) {
 		return def;
 	} else if (typeof time == "string") {
 		if (time.match(/ms$/) || time.match(/s$/)) 
@@ -105,9 +106,14 @@ Anima.parseTime = function(time, def) {
 	}
 };
 
+
+// Takes in a param and parse them to the correct unit based on the css property
+// that it is modifying
 Anima.parseParams = function(value, key) {
 	var units, defaultUnit;
 	switch(key) {
+		case "width":
+		case "height":
 		case "left":
 		case "top":
 			units = ["px", "em", "pt"];
@@ -135,6 +141,10 @@ Anima.parseParams = function(value, key) {
 	}
 };
 
+// This is the general queue;
+Anima.operations = [];
+
+
 
 // The Animation object. This is the smallest unit in the Anima library. Every 
 // performed animation per object is represented as one Animation unit.
@@ -161,20 +171,53 @@ Anima.Animation = function(el, properties, duration, delay, callback) {
 	this.properties = parseResults.styles;
 	
 	this.callback = callback;
-	
+
 	// This code sets the element with the current CSS property. This is nessary
 	// because Firefox requires an origin value otherwise it will not animate
+	var computed = document.defaultView.getComputedStyle(this.element, null);
 	for(var i = 0; i < parseResults.keys.length; i++) {
 		var key = parseResults.keys[i];
-		var d = document.defaultView.getComputedStyle(this.element, null).getPropertyValue(key);	
-		console.log(key + ", " + d);
-		this.element.style[key] = d; 
+		console.log("style: " + this.element.style[key]);
+		if (!this.element.style[key]) {
+			if (this.element.currentStyle) {
+				console.log("a: " + this.element.currentStyle[key]);
+				this.element.style[key] = this.element.currentStyle[key];
+			} else {
+				console.log("b: " + computed.getPropertyValue(key));
+				this.element.style[key] = computed.getPropertyValue(key);
+			}
+		}
 	}
 };
 
 
 // This is where the animation is actually executed
-Anima.Animation.prototype.run = function(_innerCallback) {
+Anima.Animation.prototype.run = function(_innerCallback) {	
+	// Fixes the flickering in Chrome
+	// TODO need to figure out how to better integrate
+	this.element.style[Anima.Constants.BackfaceVisibility] = "hidden";
+	
+	if (this.callback || _innerCallback) {
+		console.log("setting up callback for " + Anima.Constants.TransitionEnd);
+		
+		var _callback = this.callback;
+		
+		var _el = this.element;
+		var callbackHandler = function(el, dur) {
+			console.log("calling back");
+			console.log(el);
+			console.log(dur);
+			_el.removeEventListener(Anima.Constants.TransitionEnd, callbackHandler);
+			// Remove the animation definition here
+			_el.style[Anima.Constants.TransitionProperty] = "";
+			_el.style[Anima.Constants.TransitionDuration] = "";
+			_el.style[Anima.Constants.TransitionDelay] = "";
+			if(_callback) _callback.call(_el);
+			if(_innerCallback) setTimeout(_innerCallback, 0);
+		};
+		this.element.addEventListener(Anima.Constants.TransitionEnd, callbackHandler);
+	}
+
 	for (var key in this.style) {
 		console.log("executing " + key + ": " + this.style[key]);
 		this.element.style[key] = this.style[key];
@@ -184,46 +227,41 @@ Anima.Animation.prototype.run = function(_innerCallback) {
 		console.log("executing " + css + ": " + this.properties[css]);
 		this.element.style[css] = this.properties[css];
 	}
-	
-	if (this.callback || _innerCallback) {
-		console.log("setting up callback for " + Anima.Constants.TransitionEnd);
-		
-		var _callback = this.callback;
-		
-		var _el = this.element;
-		var callbackHandler = function() {
-			_el.removeEventListener(Anima.Constants.TransitionEnd, callbackHandler);
-			// Remove the animation definition here
-			_el.style[Anima.Constants.TransitionProperty] = "";
-			_el.style[Anima.Constants.TransitionDuration] = "";
-			_el.style[Anima.Constants.TransitionDelay] = "";
-			if(_callback) _callback();
-			if(_innerCallback) setTimeout(_innerCallback, 0);
-		};
-		this.element.addEventListener(Anima.Constants.TransitionEnd, callbackHandler);
-	}
 };
 
 
+
+
+// A frame is an object that contains multiple operations that are to be executed
+// concurrently.
 Anima.Frame = function(callback) {
 	this.operations = [];
 	this.callback = callback;
 };
 
+// The frame exposes the same methods as the base object. However, the queue method
+// only adds to the current frame.
 Anima.Frame.prototype.queue = function(el, options, duration, delay, callback) {
 	this.operations.push(
 		new Anima.Animation(el, options, duration, delay, callback)		
 	);
+	return this;
 };
 
+// The frame object also exposes a run method. The run method runs all the animations
+// in the frame concurrently
 Anima.Frame.prototype.run = function(runCallback) {
 	var opLen = this.operations.length;
 	var cbLen = opLen;
+	console.log("running " + cbLen);
 	var frameCallback = this.callback;
 	var concurrentCB = function() {
+		console.log("run concurrent callback " + cbLen);
 		if ((--cbLen) === 0 && (frameCallback || runCallback)) {
 			if (frameCallback) {
-				frameCallback();
+				setTimeout(function() {
+					frameCallback();
+				}, 20);
 			}
 			if (runCallback) {
 				setTimeout(function() {
@@ -239,51 +277,101 @@ Anima.Frame.prototype.run = function(runCallback) {
 };
 
 
-// This is the general queue;
-Anima.operations = [];
+// The complete method adds a callback when the animation is complete. One of the
+// reasons this is done is so that the API looks cleaner
+Anima.Frame.prototype.complete = function(callback) {
+	this.callback = callback;	
+};
 
+
+// The queue function. An animation sequence is defined and then queued. The
+// animation is not played immediately unti the play() function is called.
 Anima.queue = function(el, options, duration, delay, callback) {
 	if (el instanceof Anima.Frame) {
 		Anima.operations.push(el);
 	} else {
-		Anima.operations.push(
+		if (el.length && el.length > 1) {
+			console.log(typeof delay);
+			if (typeof delay == "function") {
+				callback = delay;
+				delay = null;
+			}
+			var frame = new Anima.Frame();
+			for (var i = 0; i < el.length; i++) {
+				var _el = el[i];
+				frame.queue(_el, options, duration, delay);
+			}
+			console.log(callback);
+			frame.complete(callback);
+			Anima.operations.push(frame);
+		} else {
+			Anima.operations.push(
 				new Anima.Animation(el, options, duration, delay, callback)
-				);	
+			);	
+		}
 	}
 	return Anima;
 };
 
-Anima.play = function() {
-	console.log("running");
-	var animations = Anima.operations;
-	Anima.operations = [];
-	(function next() {
-		if(animations.length === 0) return;
-		var animation = animations.shift();
-		animation.run(next);
-	})();
-};
-
-Anima.concurrent = function(callback) {
-	var opLen = Anima.operations.length;
-	var cbLen = opLen;
-	var concurrentCB = function() {
-		if ((--cbLen) === 0 && callback)	callback.call(this);
-	};
-	for (var i = 0; i < opLen; i++) {
-		var animation = Anima.operations.shift();
-		animation.run(concurrentCB);
+// This is a convenience function to create a frame of animation. It takes in an
+// array of objects:
+//
+// {
+//     element: (Dom Element)
+//     properties: (Object of key value pairs)
+//     duration: (time in s or ms)
+//     delay: (time in s or ms)
+//     callback: (callback function
+// }
+//
+// @returns the frame object
+Anima.frame = function(operations, callback) {
+	console.log("framing");
+	var frame = new Anima.Frame();
+	for (var i = 0; i < operations.length; i++) {
+		var op = operations[i];
+		frame.queue(op.element, op.properties, op.duration, op.delay, op.callback);
 	}
+	frame.complete(callback);
+	Anima.operations.push(frame);
+	return frame;
 };
 
+// Play all the animations currently in the queue, sequentially.
+Anima.play = function() {
+	async(function() {
+		var animations = Anima.operations;
+		Anima.operations = [];
+		(function next() {
+			console.log("running next");
+			if(animations.length === 0) return;
+			var animation = animations.shift();
+			animation.run(next);
+		})();	
+	});
+};
+
+// Plays only one animation in the queue;
 Anima.step = function(callback) {
-	if (Anima.operations.length === 0) return;
-	var animation = Anima.operations.shift();
-	console.log(animation);
-	animation.run(callback);
+	async(function() {
+		if (Anima.operations.length === 0) return;
+		var animation = Anima.operations.shift();
+		animation.run(callback);
+	});
 };
 
+// Plays one animation immediately
 Anima.animate = function(el, options, duration, delay, callback) {
 	Anima.queue(el, options, duration, delay, callback);
 	Anima.step();
+};
+
+// Empties the queue. Anima animations in progress cannot be canceled. However, 
+// any animations currently in the queue can be emptied.
+Anima.empty = function() {
+	Anima.operations = [];
+};
+
+var async = function(fn) {
+	setTimeout(fn, 10);
 };
